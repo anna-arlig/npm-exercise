45b147751054431d8b02f4fdaacd531c7df2613d
anna-arlig <anna.arlig@iths.se>
2022-11-11 14:54:42 +0100

    Add timeHelper function

--- date-and-time-helpers/src/timeHelper.mjs
+++ date-and-time-helpers/src/timeHelper.mjs
@@ -1,4 +1,19 @@
+import dayjs from 'dayjs'
+  import 'dayjs/locale/en-gb.js'
+  dayjs.locale('en-gb') // en-gb starts week on monday, en does not
+  const yyyymmdd = 'YYYY-MM-DD' // useful for simple .format(yyyymmdd)
+
 const timeHelper = ({startDate, endDate}) => {
-	return 'November'
+
+	let daysOfMonth = dayjs().daysInMonth(endDate)
+
+	if(dayjs(startDate).format('M') === dayjs(endDate).format('M')){
+		if(dayjs(startDate).format('D') === '1' && +dayjs(endDate).format('D') - 1  === daysOfMonth){
+			return dayjs(startDate).format('MMMM')
+		}
+		return "No valid period"
+	}
 }
+
+console.log(timeHelper({startDate: '2001-01-01', endDate: '2001-01-29'}))
 
